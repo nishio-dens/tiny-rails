157e43fe26467ceaab98e0345a0fabfc2a2a3ee0
Shinsuke Nishio <nishio@densan-labs.net>
2020-01-12 21:09:41 +0900

    engineの中にいなかったので修正

--- tiny_railties/lib/tiny_rails/engine.rb
+++ tiny_railties/lib/tiny_rails/engine.rb
@@ -2,27 +2,26 @@ require "tiny_actionpack/lib/tiny_action_dispatch/routing/route_set"
 
 module TinyRails
   class Engine
-  end
+    # Rackから呼び出される
+    def call(env)
+      app.call(env)
+    end
 
-  # Rackから呼び出される
-  def call(env)
-    app.call(env)
-  end
+    def app
+      @app ||= begin
+                 # TODO: のちのちmiddlewareをここで入れる
+                 endpoint
+               end
+    end
 
-  def app
-    @app ||= begin
-               # TODO: のちのちmiddlewareをここで入れる
-               endpoint
-             end
-  end
+    def endpoint
+      routes
+    end
 
-  def endpoint
-    routes
-  end
-
-  def routes(&block)
-    # TODO: 今後実装 Blockを渡す
-    @routes ||= TinyActionDispatch::Routing::RouteSet.new
-    @routes
+    def routes(&block)
+      # TODO: 今後実装 Blockを渡す
+      @routes ||= TinyActionDispatch::Routing::RouteSet.new
+      @routes
+    end
   end
 end

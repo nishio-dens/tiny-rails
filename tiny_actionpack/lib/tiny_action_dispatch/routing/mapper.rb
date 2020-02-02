cca272e12d2923d3c5226a87257100394d4291c5
Shinsuke Nishio <nishio@densan-labs.net>
2020-02-02 16:37:00 +0900

    No commit messages.

---
+++ tiny_actionpack/lib/tiny_action_dispatch/routing/mapper.rb
@@ -0,0 +1,9 @@
+module ActionDispatch
+  module Routing
+    class Mapper
+      def initialize(set)
+        @set = set
+      end
+    end
+  end
+end
--- tiny_actionpack/lib/tiny_action_dispatch/routing/route_set.rb
+++ tiny_actionpack/lib/tiny_action_dispatch/routing/route_set.rb
@@ -2,12 +2,19 @@ module TinyActionDispatch
   module Routing
     class RouteSet
       def draw(&block)
-        # TODO: 今後実装する
+        eval_block(block)
       end
 
       def call(env)
         # TODO: 今後実装する、今は何が来てもHelloと返す
         [200, { "Hello-TinyRails" => "Hello" }, ["Hello TinyRails!"]]
+      end
+
+      private
+
+      def eval_block(block)
+        mapper = Mapper.new(self)
+        mapper.instance_exec(&block)
       end
     end
   end
